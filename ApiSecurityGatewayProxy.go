56d9d05b7356a2c5e3adff54febd785e9b544c69
Andrew Rea <email@andrewrea.co.uk>
2016-10-25 10:58:52 +0000

    Extracted the function to create a PolicySet from a JSONPolicyDto array

--- ApiSecurityGatewayProxy.go
+++ ApiSecurityGatewayProxy.go
@@ -43,105 +43,13 @@ func (instance APISecurityGatewayProxy) ControlHandler() http.HandlerFunc {
 		}
 
 		result := proxy.Delegate(ctx, func(p HTTPProxyAPI_delegate_Params) error {
-			_, seg, err := capnp.NewMessage(capnp.SingleSegment(nil))
-			if err != nil {
-				panic(err)
-			}
-			policySet, err := NewPolicySet(seg)
+			policySet, err := NewPolicySetFromPolicyJSONDtos(policies)
 			CheckError(err)
-
-			policyList, err := NewPolicy_List(seg, int32(len(policies)))
-			CheckError(err)
-
-			for index, jsonPolicy := range policies {
-				policy, err := NewPolicy(seg)
-				CheckError(err)
-
-				policy.SetPath(jsonPolicy.Path)
-
-				verbList, err := capnp.NewTextList(seg, int32(len(jsonPolicy.Verbs)))
-				CheckError(err)
-
-				for verbIndex, verb := range jsonPolicy.Verbs {
-					verbList.Set(verbIndex, verb)
-				}
-
-				policy.SetVerbs(verbList)
-
-				headerKeyValueList, err := NewKeyValuePolicy_List(seg, int32(len(jsonPolicy.Headers)))
-				CheckError(err)
-
-				headerCount := 0
-				for headerKey, headerValues := range jsonPolicy.Headers {
-					keyValuePolicy, err := NewKeyValuePolicy(seg)
-					CheckError(err)
-
-					keyValuePolicy.SetKey(headerKey)
-
-					headerValueList, err := capnp.NewTextList(seg, int32(len(headerValues)))
-					CheckError(err)
-
-					for index, headerValue := range headerValues {
-						log.WithFields(log.Fields{
-							"headerKey":   headerKey,
-							"headerValue": headerValue,
-						}).Debug("adding headers to policy")
-						headerValueList.Set(index, headerValue)
-					}
-
-					keyValuePolicy.SetValues(headerValueList)
-					headerKeyValueList.Set(headerCount, keyValuePolicy)
-					headerCount++
-				}
-				policy.SetHeaders(headerKeyValueList)
-
-				queryKeyValueList, err := NewKeyValuePolicy_List(seg, int32(len(jsonPolicy.Query)))
-				CheckError(err)
-
-				queryCount := 0
-
-				log.WithFields(log.Fields{
-					"numberOfFields": len(jsonPolicy.Query),
-				}).Debug("About to iterate over Query")
-
-				for queryKey, queryValues := range jsonPolicy.Query {
-					keyValuePolicy, err := NewKeyValuePolicy(seg)
-					CheckError(err)
-
-					keyValuePolicy.SetKey(queryKey)
-
-					queryValueList, err := capnp.NewTextList(seg, int32(len(queryValues)))
-					CheckError(err)
-
-					log.WithFields(log.Fields{
-						"queryKey": queryKey,
-					}).Debug("query key")
-
-					for index, queryValue := range queryValues {
-						log.WithFields(log.Fields{
-							"queryValue": queryValue,
-						}).Debug("query value for key")
-						queryValueList.Set(index, queryValue)
-					}
-
-					keyValuePolicy.SetValues(queryValueList)
-					queryKeyValueList.Set(queryCount, keyValuePolicy)
-					queryCount++
-				}
-				policy.SetQuery(queryKeyValueList)
-
-				policyList.Set(index, policy)
-			}
-
-			policySet.SetPolicies(policyList)
-
 			log.WithFields(log.Fields(policySet.Map())).Debug("sending delegation")
-
 			return p.SetScope(policySet)
 		}).Key()
 
 		key, _ := result.Struct()
-
 		w.WriteHeader(int(201))
 
 		keyValue, err := key.Value()
--- PolicyJsonDto.go
+++ PolicyJsonDto.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"io"
+
+	log "github.com/Sirupsen/logrus"
+	capnp "zombiezen.com/go/capnproto2"
 )
 
 //PolicyJSONDto is used with the APIGatewayProxy Server in order to create delegations over HTTP.
@@ -23,4 +26,102 @@ func DecodePolicyJSONDtos(body io.ReadCloser) []PolicyJSONDto {
 	CheckError(err)
 
 	return policies
+}
+
+//NewPolicySetFromPolicyJSONDtos create a new PolicySet from an array of PolicyJSONDto
+func NewPolicySetFromPolicyJSONDtos(policies []PolicyJSONDto) (PolicySet, error) {
+
+	_, seg, err := capnp.NewMessage(capnp.SingleSegment(nil))
+	if err != nil {
+		panic(err)
+	}
+	policySet, err := NewPolicySet(seg)
+	CheckError(err)
+
+	policyList, err := NewPolicy_List(seg, int32(len(policies)))
+	CheckError(err)
+
+	for index, jsonPolicy := range policies {
+		policy, err := NewPolicy(seg)
+		CheckError(err)
+
+		policy.SetPath(jsonPolicy.Path)
+
+		verbList, err := capnp.NewTextList(seg, int32(len(jsonPolicy.Verbs)))
+		CheckError(err)
+
+		for verbIndex, verb := range jsonPolicy.Verbs {
+			verbList.Set(verbIndex, verb)
+		}
+
+		policy.SetVerbs(verbList)
+
+		headerKeyValueList, err := NewKeyValuePolicy_List(seg, int32(len(jsonPolicy.Headers)))
+		CheckError(err)
+
+		headerCount := 0
+		for headerKey, headerValues := range jsonPolicy.Headers {
+			keyValuePolicy, err := NewKeyValuePolicy(seg)
+			CheckError(err)
+
+			keyValuePolicy.SetKey(headerKey)
+
+			headerValueList, err := capnp.NewTextList(seg, int32(len(headerValues)))
+			CheckError(err)
+
+			for index, headerValue := range headerValues {
+				log.WithFields(log.Fields{
+					"headerKey":   headerKey,
+					"headerValue": headerValue,
+				}).Debug("adding headers to policy")
+				headerValueList.Set(index, headerValue)
+			}
+
+			keyValuePolicy.SetValues(headerValueList)
+			headerKeyValueList.Set(headerCount, keyValuePolicy)
+			headerCount++
+		}
+		policy.SetHeaders(headerKeyValueList)
+
+		queryKeyValueList, err := NewKeyValuePolicy_List(seg, int32(len(jsonPolicy.Query)))
+		CheckError(err)
+
+		queryCount := 0
+
+		log.WithFields(log.Fields{
+			"numberOfFields": len(jsonPolicy.Query),
+		}).Debug("About to iterate over Query")
+
+		for queryKey, queryValues := range jsonPolicy.Query {
+			keyValuePolicy, err := NewKeyValuePolicy(seg)
+			CheckError(err)
+
+			keyValuePolicy.SetKey(queryKey)
+
+			queryValueList, err := capnp.NewTextList(seg, int32(len(queryValues)))
+			CheckError(err)
+
+			log.WithFields(log.Fields{
+				"queryKey": queryKey,
+			}).Debug("query key")
+
+			for index, queryValue := range queryValues {
+				log.WithFields(log.Fields{
+					"queryValue": queryValue,
+				}).Debug("query value for key")
+				queryValueList.Set(index, queryValue)
+			}
+
+			keyValuePolicy.SetValues(queryValueList)
+			queryKeyValueList.Set(queryCount, keyValuePolicy)
+			queryCount++
+		}
+		policy.SetQuery(queryKeyValueList)
+
+		policyList.Set(index, policy)
+	}
+
+	policySet.SetPolicies(policyList)
+
+	return policySet, nil
 }
